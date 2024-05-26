84f49d51bc1887b928093d8edf9de54f057bb3c2
jaimerod156 <jaimerodriguez156@gmail.com>
2024-05-26 09:01:08 -0400

    Inicio menu desplegable, toggle, Final

---
+++ 5 Menu desplegable/script.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.onclick =() =>{
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
+
+
+
