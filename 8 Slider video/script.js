a3bfc312dbeaacf9af1715bb75248b787ce31b2d
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-02 08:26:33 -0400

    Repaso maquetacion, estilos y javascript inicial

---
+++ 8 Slider video/script.js
@@ -0,0 +1,15 @@
+
+function ToggleMenu(){
+    const menuIcon = document.querySelector(".menu-icon");
+    menuIcon.classList.toggle('active');
+
+    const navBar = document.getElementById("nav-bar");
+    navBar.classList.toggle("active")
+
+}
+
+const sliderShow = document.getElementById("slider-show");
+const slides= document.getElementsByTagName("video");
+
+
+
