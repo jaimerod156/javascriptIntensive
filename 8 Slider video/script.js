a9703b235a24133955c40d1e0b4b662889df4b87
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-03 15:04:57 -0400

    Revision Final

--- 8 Slider video/script.js
+++ 8 Slider video/script.js
@@ -1,15 +1,40 @@
 
-function ToggleMenu(){
-    const menuIcon = document.querySelector(".menu-icon");
-    menuIcon.classList.toggle('active');
+function ToggleMenu() {
+  const menuIcon = document.querySelector(".menu-icon");
+  menuIcon.classList.toggle("active");
 
-    const navBar = document.getElementById("nav-bar");
-    navBar.classList.toggle("active")
-
+  const navBar = document.getElementById("nav-bar");
+  navBar.classList.toggle("active");
 }
 
 const sliderShow = document.getElementById("slider-show");
-const slides= document.getElementsByTagName("video");
+const slides = sliderShow.getElementsByTagName("video");
+const sliderShowText = document.getElementById("slider-show-text");
+const sliderText = sliderShowText.getElementsByTagName("div");
 
+let index = 0;
 
+function nextSlider() {
+  slides[index].classList.remove("active");
+  sliderText[index].classList.remove("active");
+  // index++;
+  // if(index < slides.length){
+  //     slides[index].classList.add('active');
+  //     console.log(index);    }
+  // else {
+  //     index = 0;
+  //     slides[index].classList.add('active');
+  // }
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+  sliderText[index].classList.add("active");
+}
 
+function prevSlider() {
+  slides[index].classList.remove("active");
+  sliderText[index].classList.remove("active");
+
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+  sliderText[index].classList.add("active");
+}
