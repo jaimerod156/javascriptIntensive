2cc60549afd9f4fdbca8c6d8e791e4c83c43bc9c
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-07 08:47:11 -0400

    Inicio proyecto Parallex

---
+++ 11 Parallax efect/script.js
@@ -0,0 +1,5 @@
+let scene = document.getElementById('scene');
+let text = document.getElementById('text');
+let parallaxInstScene = new Parallax(scene);
+let parallaxTInstext = new Parallax(text);
+
