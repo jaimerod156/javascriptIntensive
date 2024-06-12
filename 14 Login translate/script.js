35e850d9dffb04eea4fae17bb1414b3ca712f726
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-12 08:51:04 -0400

    Final funcionalidad javascript y animaciones css

--- 14 Login translate/script.js
+++ 14 Login translate/script.js
@@ -3,5 +3,9 @@ const btnRegistrarse = document.getElementById("sign-up-btn");
 const btnIniciarSesion = document.getElementById("sign-in-btn");
 
 btnRegistrarse.addEventListener("click",()=>{
-    container.classList.add("sign-up-mode")
+    container.classList.add("sign-up-mode");
+})
+
+btnIniciarSesion.addEventListener('click',()=>{
+    container.classList.remove('sign-up-mode');
 })
