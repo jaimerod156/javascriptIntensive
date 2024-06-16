ace2d2255fdb279af5e1835a50b04ecbf78fd3f2
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-16 09:09:31 -0400

    Fin estilos css

--- 15 Pokedex/app.js
+++ 15 Pokedex/app.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let geners = ["generation-1",
+document.addEventListener("DOMContentLoaded",function(){
+    var geners = [
+        "generation-1",
         "generation-2",
         "generation-3",
         "generation-4",
         "generation-5",
         "generation-6",
-        "generation-7"
-    ];
+        "generation-7",
+      ];
     let filters = document.getElementById('filters');
-    let gen = '';
-    let value = 1;
-    for(let gener of geners){
-        gen+=`
-        <input class="radio-gen" type="radio" id=${gener} value=${value} name="generation" checked>
-        <label for=${gener} class="label-generation">${gener}</label>
+    let gen = "";
+    for(let i=0; i < geners.length;i++){
+        gen +=`
+        <input class="radio-gen" type="radio" id=${geners[i]} value=${i+1} name="generation" checked>
+        <label for=${geners[i]} class="label-generation">${geners[i]}</label>
         `
-        value++;
-        
     }
     filters.innerHTML = gen;
 })
