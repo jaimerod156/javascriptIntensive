3f2925a56d9f5785b4be93cc9a2057eacbc33ffa
jaimerod156 <jaimerodriguez156@gmail.com>
2024-06-25 07:27:04 -0400

    Ajustes de maquetacion, imagenes y estilos

---
+++ 16 Halloween Page/assets/js/main.js
@@ -0,0 +1,28 @@
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+        console.log('menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu');
+
+    })
+}
+
+// Scroll reveal
+const sr =ScrollReveal({
+    origin:"top",
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+}) 
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+
