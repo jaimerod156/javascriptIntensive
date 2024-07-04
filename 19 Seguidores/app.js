bf330a5f85b1f980e280d0386322962440a5e6f5
jaimerod156 <jaimerodriguez156@gmail.com>
2024-07-04 08:21:34 -0400

    Inicio Contador seguidores, javaScript

---
+++ 19 Seguidores/app.js
@@ -0,0 +1,19 @@
+const counterEl = document.querySelectorAll('.counter');
+
+counterEl.forEach((item) =>{
+    item.innerText = '0';
+    const target = +item.getAttribute('data-target');
+    const interval = target/100
+
+    const updateCounter = () =>{
+        const value = +item.innerText;
+        if(value < target){
+            item.innerText = Math.ceil(value + interval);
+            setTimeout(()=>{
+                updateCounter()
+            },50)
+        }
+    }
+
+    updateCounter();
+})
